252d580baddfc41ecb261c8c485111976087bb9d
leviszz <levidias2007@gmail.com>
2025-10-22 13:39:09 -0300

    Retirada de codigo script.js duplo do repositorio

--- site-alistamento/servicos.js
+++
@@ -1,9 +0,0 @@
-
-        function closeMobileMenu(){ 
-            const menu = document.getElementById('mobileMenu');
-            if(menu) menu.classList.remove('open');
-        }
-        document.getElementById('mobileMenuBtn')?.addEventListener('click', function(){
-            document.getElementById('mobileMenu')?.classList.toggle('open');
-        });
-    
